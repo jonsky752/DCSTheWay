6c72b0a8a4964cc5047f9efa122f7d5b1a3fa5ff
jonsky752 <jonsky7@ntlworld.com>
2024-09-03 19:19:26 +0100

    Add files via upload
    Changes to enable CH47 to automatically create a flight plan from the saved waypoint

---
+++ GetModuleCommands.js
@@ -0,0 +1,105 @@
+import f16 from "./f16";
+import f15e from "./f15e";
+import fa18 from "./fa18";
+import ah64 from "./ah64";
+import a10 from "./a10";
+import m2000 from "./m2000";
+import av8b from "./av8b";
+import ka50 from "./ka50";
+import miragef1 from "./miragef1";
+import uh60l from "./uh60l";
+import hercules from "./Hercules";
+import oh58d from "./oh58d";
+import ch47f from "./ch47f";
+
+export default function getModuleCommands(module, waypoints, buttonExtraDelay) {
+  switch (module) {
+    case "F-15ESE_pilotA":
+    case "F-15ESE_wsoA":
+    case "F-15ESE_pilotB":
+    case "F-15ESE_wsoB":
+      f15e.slotVariant = module;
+      f15e.extraDelay = buttonExtraDelay;
+      return f15e.createButtonCommands(waypoints);
+    case "F-16C_50":
+    case "F-16D_50":
+    case "F-16D_50_NS":
+    case "F-16D_52":
+    case "F-16D_52_NS":
+    case "F-16D_Barak_30":
+    case "F-16D_Barak_40":
+    case "F-16I": {
+      f16.extraDelay = buttonExtraDelay;
+      return f16.createButtonCommands(waypoints);
+    }
+    case "FA-18C_hornet":
+    case "FA-18E":
+    case "FA-18F":
+    case "EA-18G": {
+      fa18.extraDelay = buttonExtraDelay;
+      return fa18.createButtonCommands(waypoints);
+    }
+    case "AH-64D_BLK_IIpilot": {
+      ah64.extraDelay = buttonExtraDelay;
+      ah64.slotVariant = "AH-64D_BLK_IIpilot";
+      return ah64.createButtonCommands(waypoints);
+    }
+    case "AH-64D_BLK_IIgunner": {
+      ah64.extraDelay = buttonExtraDelay;
+      ah64.slotVariant = "AH-64D_BLK_IIgunner";
+      return ah64.createButtonCommands(waypoints);
+    }
+    case "A-10C_2":
+    case "A-10C": {
+      a10.extraDelay = buttonExtraDelay;
+      return a10.createButtonCommands(waypoints);
+    }
+    case "M-2000C": {
+      m2000.extraDelay = buttonExtraDelay;
+      return m2000.createButtonCommands(waypoints);
+    }
+    case "AV8BNA": {
+      av8b.extraDelay = buttonExtraDelay;
+      return av8b.createButtonCommands(waypoints);
+    }
+    case "Ka-50":
+    case "Ka-50_3": {
+      ka50.extraDelay = buttonExtraDelay;
+      return ka50.createButtonCommands(waypoints);
+    }
+    case "Mirage-F1EE": {
+      miragef1.extraDelay = buttonExtraDelay;
+      return miragef1.createButtonCommands(waypoints);
+    }
+    case "UH-60L": {
+      uh60l.extraDelay = buttonExtraDelay;
+      return uh60l.createButtonCommands(waypoints);
+    }
+    case "Hercules": {
+      hercules.extraDelay = buttonExtraDelay;
+      return hercules.createButtonCommands(waypoints);
+    }
+    case "OH58Dright-seat": {
+      oh58d.extraDelay = buttonExtraDelay;
+      oh58d.slotVariant = "OH58Dright-seat";
+      return oh58d.createButtonCommands(waypoints);
+    }
+    case "OH58Dleft-seat": {
+      oh58d.extraDelay = buttonExtraDelay;
+      oh58d.slotVariant = "OH58Dleft-seat";
+      return oh58d.createButtonCommands(waypoints);
+    }
+    case "ch47ADD": {
+      ch47f.extraDelay = buttonExtraDelay;
+      ch47f.slotVariant = "ch47ADD";
+      return ch47f.createButtonCommands(waypoints);
+    }
+    case "ch47NEW": {
+      ch47f.extraDelay = buttonExtraDelay;
+      ch47f.slotVariant = "ch47NEW";
+      return ch47f.createButtonCommands(waypoints);
+    }
+    default:
+      return [];
+  }
+}
---
+++ askUserAboutSeat.js
@@ -0,0 +1,110 @@
+import { TwoOptionsDialog } from "../components/TwoOptionsDialog";
+import { AlertDialog } from "../components/AlertDialog";
+
+const askUserAboutSeat = async (module, userPreferences) => {
+  const moduleSpecificPreferences = userPreferences[module];
+
+  if (module === "AH-64D_BLK_II") {
+    if (moduleSpecificPreferences?.includes("Pilot"))
+      return "AH-64D_BLK_IIpilot";
+    else if (moduleSpecificPreferences?.includes("CPG/Gunner"))
+      return "AH-64D_BLK_IIgunner";
+    else {
+      return TwoOptionsDialog({
+        title: "What seat are you in?",
+        op1: "Pilot",
+        op2: "CPG/Gunner",
+      }).then((option) =>
+        option === "CPG/Gunner" ? "AH-64D_BLK_IIgunner" : "AH-64D_BLK_IIpilot",
+      );
+    }
+  } else if (
+    module === "FA-18C_hornet" ||
+    module === "FA-18E" ||
+    module === "FA-18F" ||
+    module === "EA-18G"
+  ) {
+    if (moduleSpecificPreferences?.includes("Hide")) return "FA-18C_hornet";
+    else {
+      return AlertDialog({
+        title: "Please make sure that",
+        content:
+          "1. PRECISE option is boxed in HSI > DATA\n" +
+          "2. You are not in the TAC menu\n" +
+          "3. You are in the 00°00.0000' coordinate format",
+      }).then(() => "FA-18C_hornet");
+    }
+  } else if (module === "F-15ESE") {
+    let seat;
+    if (moduleSpecificPreferences?.includes("Pilot")) seat = "Pilot";
+    else if (moduleSpecificPreferences?.includes("WSO")) seat = "WSO";
+    else {
+      await TwoOptionsDialog({
+        title: "What seat are you in?",
+        op1: "Pilot",
+        op2: "WSO",
+      }).then((chosenSeat) => (seat = chosenSeat));
+    }
+
+    let route;
+    if (moduleSpecificPreferences?.includes("A{1/A}")) route = "A{1/A}";
+    else if (moduleSpecificPreferences?.includes("B{1/B}")) route = "B{1/B}";
+    else {
+      await TwoOptionsDialog({
+        title: "What route are you using?",
+        op1: "A{1/A}", // op1: "A"
+        op2: "B{1/B}", // op2: "B" // removed because its a nice touch to keep the explicitness of this here, instead of making it more inline.
+      }).then((chosenRoute) => (route = chosenRoute));
+    }
+    return `F-15ESE_${seat.toLowerCase()}${route === "A{1/A}" ? "A" : "B"}`;
+  } else if (module === "UH-60L") {
+    if (moduleSpecificPreferences?.includes("Hide")) return "UH-60L";
+    else {
+      return AlertDialog({
+        title: "Be advised:",
+        content:
+          "This may overwrite waypoints! " +
+          "If WP/TGT is on MIZ0 (00), 01 onwards will be overwritten.",
+      }).then(() => "UH-60L");
+    }
+  } else if (module === "Hercules") {
+    if (moduleSpecificPreferences?.includes("Hide")) return "Hercules";
+    else {
+      return AlertDialog({
+        title: "Be advised:",
+        content:
+          "Make sure you have downloaded the Patch for the Hercules module: https://github.com/Summit60/DCS-Hercules-TheWay-patch",
+      }).then(() => "Hercules");
+    }
+  } else if (module === "OH58D") {
+    if (moduleSpecificPreferences?.includes("Right Seat"))
+      return "OH58Dright-seat";
+    else if (moduleSpecificPreferences?.includes("Left Seat"))
+      return "OH58Dleft-seat";
+    else {
+      return TwoOptionsDialog({
+        title: "What seat are you in?",
+        op1: "Right Seat",
+        op2: "Left Seat",
+      }).then((option) =>
+        option === "Left Seat" ? "OH58Dleft-seat" : "OH58Dright-seat",
+      );
+    }
+  } else if (module === "CH-47Fbl1") {
+    if (moduleSpecificPreferences?.includes("Add to FLPN"))
+      return "ch47ADD";
+    else if (moduleSpecificPreferences?.includes("Make New FPLN"))
+      return "ch47NEW";
+    else {
+      return TwoOptionsDialog({
+        title: "Would you like to?",
+        op1: "Add to FPLN",
+        op2: "Make New FPLN",
+      }).then((option) =>
+        option === "Add to FPLN" ? "ch47ADD" : "ch47NEW",
+      );
+    }
+  } else return module;
+};
+
+export default askUserAboutSeat;
