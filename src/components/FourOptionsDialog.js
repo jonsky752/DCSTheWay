22b221a91bd77c38508f7d08c33a7eaee35a54cf
jonsky752 <jonsky7@ntlworld.com>
2026-01-03 14:13:34 +0000

    2.5.12
    Update for "Remember this choice"
    FA-18/A
    CH47F

--- src/components/FourOptionsDialog.js
+++ src/components/FourOptionsDialog.js
@@ -1,4 +1,108 @@
 import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogTitle,
+  FormControlLabel,
+  FormGroup,
+  Stack,
+} from "@mui/material";
+import { createModal } from "react-modal-promise";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../store/ui";
+import { saveModulePreferences } from "../utils/savePreferences";
+
+/**
+ * Four-option dialog WITH "Remember this choice"
+ */
+const FourDialog = ({
+  isOpen,
+  onResolve,
+  onReject,
+  title,
+  op1,
+  op2,
+  op3,
+  op4,
+}) => {
+  const { module } = useSelector((state) => state.dcsPoint);
+  const [rememberChoice, setRememberChoice] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleRememberChoice = (event) => {
+    setRememberChoice(event.target.checked);
+  };
+
+  const handleOptionSelected = (option) => {
+    if (rememberChoice) {
+      const choice = { module, option };
+      dispatch(uiActions.setModulePreference(choice));
+      saveModulePreferences(choice);
+    }
+    onResolve(option);
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={onReject}>
+      <DialogTitle sx={{ textAlign: "center" }}>{title}</DialogTitle>
+      <Stack>
+        <Button onClick={() => handleOptionSelected(op1)}>{op1}</Button>
+        <Button onClick={() => handleOptionSelected(op2)}>{op2}</Button>
+        <Button onClick={() => handleOptionSelected(op3)}>{op3}</Button>
+        <Button onClick={() => handleOptionSelected(op4)}>{op4}</Button>
+
+        <FormGroup sx={{ alignItems: "center" }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={rememberChoice}
+                onChange={handleRememberChoice}
+              />
+            }
+            label="Remember this choice"
+          />
+        </FormGroup>
+      </Stack>
+    </Dialog>
+  );
+};
+
+/**
+ * Four-option dialog WITHOUT remembering (simple / transient)
+ */
+const FourSimpleDialog = ({
+  isOpen,
+  onResolve,
+  onReject,
+  title,
+  op1,
+  op2,
+  op3,
+  op4,
+}) => {
+  const handleOptionSelected = (option) => {
+    onResolve(option);
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={onReject}>
+      <DialogTitle sx={{ textAlign: "center" }}>{title}</DialogTitle>
+      <Stack>
+        <Button onClick={() => handleOptionSelected(op1)}>{op1}</Button>
+        <Button onClick={() => handleOptionSelected(op2)}>{op2}</Button>
+        <Button onClick={() => handleOptionSelected(op3)}>{op3}</Button>
+        <Button onClick={() => handleOptionSelected(op4)}>{op4}</Button>
+      </Stack>
+    </Dialog>
+  );
+};
+
+// Exports
+export const FourOptionsDialog = createModal(FourDialog);
+export const FourOptionsSimpleDialog = createModal(FourSimpleDialog);
+
+/*import {
     Button,
     Dialog,
     DialogTitle,
@@ -24,4 +128,4 @@ import {
     );
   };
 
-  export const FourOptionsSimpleDialog = createModal(FourSimpleDialog);
+  export const FourOptionsSimpleDialog = createModal(FourSimpleDialog);*/
--- src/moduleCommands/askUserAboutSeat.js
+++ src/moduleCommands/askUserAboutSeat.js
@@ -2,283 +2,244 @@ import {
   TwoOptionsDialog,
   TwoOptionsSimpleDialog,
 } from "../components/TwoOptionsDialog";
-import { FourOptionsSimpleDialog } from "../components/FourOptionsDialog";
+import {
+  FourOptionsDialog,
+  FourOptionsSimpleDialog,
+} from "../components/FourOptionsDialog";
 import { AlertDialog } from "../components/AlertDialog";
 
 const askUserAboutSeat = async (module, userPreferences) => {
   const moduleSpecificPreferences = userPreferences[module];
 
-  
-  
-  //////////////////////////////////////////////////////////////////////   AH-64D   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
+  // AH-64D
   if (module === "AH-64D_BLK_II") {
-    if (moduleSpecificPreferences?.includes("Pilot"))
-      return "AH-64D_BLK_IIpilot";
-    else if (moduleSpecificPreferences?.includes("CPG/Gunner"))
-      return "AH-64D_BLK_IIgunner";
-    else {
-      return TwoOptionsDialog({
-        title: "What seat are you in?",
-        op1: "Pilot",
-        op2: "CPG/Gunner",
-      }).then((option) =>
-        option === "CPG/Gunner" ? "AH-64D_BLK_IIgunner" : "AH-64D_BLK_IIpilot",
-      );
-    }
-  
-   //////////////////////////////////////////////////////////////////////   FA-18   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
-  
-  } else if (
-    module === "FA-18C_hornet" ||
-    module === "FA-18E" ||
-    module === "FA-18F" ||
-    module === "EA-18G"
-  ) {
-    let PPinput;
-    await TwoOptionsSimpleDialog({
+    if (moduleSpecificPreferences?.includes("Pilot")) return "AH-64D_BLK_IIpilot";
+    if (moduleSpecificPreferences?.includes("CPG/Gunner")) return "AH-64D_BLK_IIgunner";
+
+    return TwoOptionsDialog({
+      title: "What seat are you in?",
+      op1: "Pilot",
+      op2: "CPG/Gunner",
+    }).then((option) =>
+      option === "CPG/Gunner" ? "AH-64D_BLK_IIgunner" : "AH-64D_BLK_IIpilot"
+    );
+  }
+
+  /// FA-18
+else if (
+  module === "FA-18C_hornet" ||
+  module === "FA-18E" ||
+  module === "FA-18F" ||
+  module === "EA-18G"
+) {
+  // Use saved preference if it exists
+  let PPinput;
+  if (moduleSpecificPreferences?.includes("YES")) {
+    PPinput = "YES";
+  } else if (moduleSpecificPreferences?.includes("NO")) {
+    PPinput = "NO";
+  } else {
+    // Ask user and allow "remember this choice"
+    PPinput = await TwoOptionsDialog({
       title: "Input as PP MSN?",
       op1: "YES",
       op2: "NO",
-    }).then((pp) => (PPinput = pp));
+    });
+  }
 
-    let stations = "";
+  let stations = "";
+  if (PPinput === "YES") {
+    stations = await FourOptionsSimpleDialog({
+      title: "How many STATIONs carry this weapon?",
+      op1: "1",
+      op2: "2",
+      op3: "3",
+      op4: "4",
+    });
+  }
+
+  const hide = moduleSpecificPreferences?.includes("Hide") ?? false;
+
+  if (!hide) {
     if (PPinput === "YES") {
-      await FourOptionsSimpleDialog({
-        title: "How many STATIONs carry this weapon?",
-        op1: "1",
-        op2: "2",
-        op3: "3",
-        op4: "4",
-      }).then((sta) => (stations = sta));
+      await AlertDialog({
+        title: "Please make sure that",
+        content: "Your LEFT MDI is on PP MSN Page!\n",
+      });
+    } else {
+      await AlertDialog({
+        title: "Please make sure that",
+        content:
+          "1. PRECISE option is boxed in HSI > DATA\n" +
+          "2. You are not in the TAC menu\n" +
+          "3. You are in the 00°00.0000' coordinate format",
+      });
     }
-    let hide = false;
-    if (moduleSpecificPreferences?.includes("Hide")) {
-      hide = true;
+  }
+
+  return `FA-18C_hornet${PPinput === "YES" ? "PP" : ""}${stations}`;
+}
+
+  // F-15E
+  else if (module === "F-15ESE") {
+    let seat;
+    if (moduleSpecificPreferences?.includes("Pilot")) seat = "Pilot";
+    else if (moduleSpecificPreferences?.includes("WSO")) seat = "WSO";
+    else {
+      seat = await TwoOptionsDialog({
+        title: "What seat are you in?",
+        op1: "Pilot",
+        op2: "WSO",
+      });
     }
-    if (hide === false) {
-      if (PPinput === "YES") {
+
+    const jdam = await TwoOptionsSimpleDialog({
+      title: "Input for JDAMs?",
+      op1: "YES",
+      op2: "NO",
+    });
+
+    const route = jdam === "YES" ? "B{1/B}" : "A{1/A}";
+    const hide = moduleSpecificPreferences?.includes("Hide") ?? false;
+
+    if (!hide) {
+      if (seat === "Pilot" && jdam === "YES") {
         await AlertDialog({
-          title: "Please make sure that",
-          content: "Your LEFT MDI is on PP MSN Page!\n",
-        });
-      } else {
-        await AlertDialog({
-          title: "Please make sure that",
+          title: "Make sure:",
           content:
-            "1. PRECISE option is boxed in HSI > DATA\n" +
-            "2. You are not in the TAC menu\n" +
-            "3. You are in the 00°00.0000' coordinate format",
+            "1. Your RIGHT MPD is on Smart Weapons page.\n" +
+            "2. Used 'NXT STA' to select the bomb you want to start with.\n" +
+            "3. Do not program the JDAMs in PACS.(Recommended)",
+        });
+      } else if (seat === "WSO" && jdam === "YES") {
+        await AlertDialog({
+          title: "Make sure:",
+          content:
+            "1. The airplane's master mode is A/G\n" +
+            "2. Your RIGHT MPD(Green display) is on Smart Weapons page.\n" +
+            "3. Used 'NXT STA' to select the bomb you want to start with.\n" +
+            "4. Do not program the JDAMs in PACS.(Recommend)",
         });
       }
     }
-    return `FA-18C_hornet${PPinput === "YES" ? "PP" : ""}${stations}`;
 
-  
-       //////////////////////////////////////////////////////////////////////   F-15E   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    return `F-15ESE_${seat.toLowerCase()}${route === "A{1/A}" ? "A" : "B"}${
+      jdam === "YES" ? "JDAM" : "NOJDAM"
+    }`;
+  }
 
-      } else if (module === "F-15ESE") {
-        let seat;
-        if (moduleSpecificPreferences?.includes("Pilot")) seat = "Pilot";
-        else if (moduleSpecificPreferences?.includes("WSO")) seat = "WSO";
-        else {
-          await TwoOptionsDialog({
-            title: "What seat are you in?",
-            op1: "Pilot",
-            op2: "WSO",
-          }).then((chosenSeat) => (seat = chosenSeat));
-        }
-    
-        let jdam;
-        await TwoOptionsSimpleDialog({
-          title: "Input for JDAMs?",
-          op1: "YES",
-          op2: "NO",
-        }).then((forJDAM) => (jdam = forJDAM));
-    
-        let route = jdam === "YES" ? "B{1/B}" : "A{1/A}";
-    
-        let hide = false;
-        if (moduleSpecificPreferences?.includes("Hide")) {
-          hide = true;
-        }
-        if (hide === false) {
-          if (seat === "Pilot" && jdam === "YES") {
-            await AlertDialog({
-              title: "Make sure:",
-              content:
-                "1. Your RIGHT MPD is on Smart Weapons page.\n" +
-                "2. Used 'NXT STA' to select the bomb you want to start with.\n" +
-                "3. Do not program the JDAMs in PACS.(Recommended)",
-            });
-          } else if (seat === "WSO" && jdam === "YES") {
-            await AlertDialog({
-              title: "Make sure:",
-              content:
-                "1. The airplane's master mode is A/G\n" +
-                "2. Your RIGHT MPD(Green display) is on Smart Weapons page.\n" +
-                "3. Used 'NXT STA' to select the bomb you want to start with.\n" +
-                "4. Do not program the JDAMs in PACS.(Recommend)",
-            });
-          }
-        }
-    
-        return `F-15ESE_${seat.toLowerCase()}${route === "A{1/A}" ? "A" : "B"}${
-          jdam === "YES" ? "JDAM" : "NOJDAM"
-        }`;
-    
-    
-   //////////////////////////////////////////////////////////////////////   UH-60L + Hercules Mods   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-  
-  } else if (module === "UH-60L") {
+  // UH-60L
+  else if (module === "UH-60L") {
     if (moduleSpecificPreferences?.includes("Hide")) return "UH-60L";
-    else {
-      return AlertDialog({
-        title: "Be advised:",
-        content:
-          "This may overwrite waypoints! " +
-          "If WP/TGT is on MIZ0 (00), 01 onwards will be overwritten.",
-      }).then(() => "UH-60L");
-    }
-  } else if (module === "Hercules") {
+    return AlertDialog({
+      title: "Be advised:",
+      content:
+        "This may overwrite waypoints! If WP/TGT is on MIZ0 (00), 01 onwards will be overwritten.",
+    }).then(() => "UH-60L");
+  }
+
+  // Hercules
+  else if (module === "Hercules") {
     if (moduleSpecificPreferences?.includes("Hide")) return "Hercules";
-    else {
-      return AlertDialog({
-        title: "Be advised:",
-        content:
-          "Make sure you have downloaded the Patch for the Hercules module: https://github.com/Summit60/DCS-Hercules-TheWay-patch",
-      }).then(() => "Hercules");
-    }
-  
-  
-   //////////////////////////////////////////////////////////////////////   OH-58D   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    return AlertDialog({
+      title: "Be advised:",
+      content:
+        "Make sure you have downloaded the Patch for the Hercules module: https://github.com/Summit60/DCS-Hercules-TheWay-patch",
+    }).then(() => "Hercules");
+  }
 
-  
-  } else if (module === "OH58D") {
-    if (moduleSpecificPreferences?.includes("Right Seat"))
-      return "OH58Dright-seat";
-    else if (moduleSpecificPreferences?.includes("Left Seat"))
-      return "OH58Dleft-seat";
-    else {
-      return TwoOptionsDialog({
-        title: "What seat are you in?",
-        op1: "Right Seat",
-        op2: "Left Seat",
-      }).then((option) =>
-        option === "Left Seat" ? "OH58Dleft-seat" : "OH58Dright-seat",
-      );
-    }
-  
-  
-     //////////////////////////////////////////////////////////////////////   CH-47F   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
-    } else if (module === "CH-47Fbl1") {
-      if (moduleSpecificPreferences?.includes("Add to FLPN")) {
+  // OH-58D
+  else if (module === "OH58D") {
+    if (moduleSpecificPreferences?.includes("Right Seat")) return "OH58Dright-seat";
+    if (moduleSpecificPreferences?.includes("Left Seat")) return "OH58Dleft-seat";
+    return TwoOptionsDialog({
+      title: "What seat are you in?",
+      op1: "Right Seat",
+      op2: "Left Seat",
+    }).then((option) =>
+      option === "Left Seat" ? "OH58Dleft-seat" : "OH58Dright-seat"
+    );
+  }
+
+  // CH-47F
+  else if (module === "CH-47Fbl1") {
+    if (moduleSpecificPreferences?.includes("Add to FLPN")) return "ch47ADD";
+    if (moduleSpecificPreferences?.includes("Make New FLPN")) return "ch47NEW";
+    if (moduleSpecificPreferences?.includes("Add to ALT FLPN")) return "ch47ALTADD";
+    if (moduleSpecificPreferences?.includes("Make New ALT FLPN")) return "ch47ALTNEW";
+
+    const option = await FourOptionsDialog({
+      title: "Would you like to?",
+      op1: "Add to FLPN",
+      op2: "Make New FLPN",
+      op3: "Add to ALT FLPN",
+      op4: "Make New ALT FLPN",
+    });
+
+    switch (option) {
+      case "Add to FLPN":
         return "ch47ADD";
-      } else if (moduleSpecificPreferences?.includes("Make New FPLN")) {
+      case "Make New FLPN":
         return "ch47NEW";
-      } else if (moduleSpecificPreferences?.includes("Add to ALT FLPN")) {
+      case "Add to ALT FLPN":
         return "ch47ALTADD";
-      } else if (moduleSpecificPreferences?.includes("Make New FLPN")) {
+      case "Make New ALT FLPN":
         return "ch47ALTNEW";
-      } else {
-        return FourOptionsSimpleDialog({
-          title: "Would you like to?",
-          op1: "Add to FLPN",
-          op2: "Make New FLPN",
-          op3: "Add to ALT FLPN",
-          op4: "Make New ALT FLPN",
-        }).then((option) => {
-          switch (option) {
-            case "Add to FLPN":
-              return "ch47ADD";
-            case "Make New FLPN":
-              return "ch47NEW";
-            case "Add to ALT FLPN":
-              return "ch47ALTADD";
-            case "Make New ALT FLPN":
-              return "ch47ALTNEW";
-            default:
-              throw new Error("Invalid option selected");
-          }
-        });
-      }
-  
-//////////////////////////////////////////////////////////////////////   C130J  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-   } else if (module === "C-130J-30") {
-    if (moduleSpecificPreferences?.includes("Left Seat"))
-      return "c130Plt";
-    else if (moduleSpecificPreferences?.includes("Right Seat"))
-      return "c130CoPlt";
-    else {
-      return TwoOptionsDialog({
-        title: "What seat are you in?",
-        op1: "Left Seat",
-        op2: "Right Seat",
-      }).then((option) =>
-        option === "Left Seat" ? "c130Plt" : "c130CoPlt",
-      );
+      default:
+        throw new Error("Invalid option selected");
     }
-  
-   //////////////////////////////////////////////////////////////////////   A-10C/2   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  }
 
-  
-  } else if (module === "A-10C" ||  module === "A-10C_2") {
-    if (moduleSpecificPreferences?.includes("Add Waypoints"))
-      return "a10ADD";
-    else if (moduleSpecificPreferences?.includes("Overwrite Waypoints"))
-      return "a10NEW";
-    else {
-      return TwoOptionsDialog({
-        title: "Would you like to?",
-        op1: "Add Waypoints",
-        op2: "Overwrite Waypoints",
-      }).then((option) =>
-        option === "Add Waypoints" ? "a10ADD" : "a10NEW",
-      );
-    }  
-   
-  
-    //////////////////////////////////////////////////////////////////////   AV8BNA   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  // C-130J
+  else if (module === "C-130J-30") {
+    if (moduleSpecificPreferences?.includes("Left Seat")) return "c130Plt";
+    if (moduleSpecificPreferences?.includes("Right Seat")) return "c130CoPlt";
+    const option = await TwoOptionsDialog({
+      title: "What seat are you in?",
+      op1: "Left Seat",
+      op2: "Right Seat",
+    });
+    return option === "Left Seat" ? "c130Plt" : "c130CoPlt";
+  }
 
-  
-  } else if (module === "AV8BNA") {
-      if (moduleSpecificPreferences?.includes("Waypoints"))
-        return "AV8BNA_WPT";
-      else if (moduleSpecificPreferences?.includes("Targetpoints"))
-        return "AV8BNA_TRGPT";
-      else {
-        return TwoOptionsDialog({
-          title: "Transfer to waypoints or target points?",
-          op1: "Waypoints",
-          op2: "Targetpoints",
-        }).then((option) =>
-          option === "Targetpoints" ? "AV8BNA_TRGPT" : "AV8BNA_WPT",
-      );
-    }
+  // A-10C/2
+  else if (module === "A-10C" || module === "A-10C_2") {
+    if (moduleSpecificPreferences?.includes("Add Waypoints")) return "a10ADD";
+    if (moduleSpecificPreferences?.includes("Overwrite Waypoints")) return "a10NEW";
+    const option = await TwoOptionsDialog({
+      title: "Would you like to?",
+      op1: "Add Waypoints",
+      op2: "Overwrite Waypoints",
+    });
+    return option === "Add Waypoints" ? "a10ADD" : "a10NEW";
+  }
 
-    //////////////////////////////////////////////////////////////////////   Ka-50   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  // AV8BNA
+  else if (module === "AV8BNA") {
+    if (moduleSpecificPreferences?.includes("Waypoints")) return "AV8BNA_WPT";
+    if (moduleSpecificPreferences?.includes("Targetpoints")) return "AV8BNA_TRGPT";
+    const option = await TwoOptionsDialog({
+      title: "Transfer to waypoints or target points?",
+      op1: "Waypoints",
+      op2: "Targetpoints",
+    });
+    return option === "Targetpoints" ? "AV8BNA_TRGPT" : "AV8BNA_WPT";
+  }
 
-  } else if (module === "Ka-50" || module === "Ka-50_3") {
-    if (moduleSpecificPreferences?.includes("Waypoints"))
-      return "ka50WPT";
-    else if (moduleSpecificPreferences?.includes("Targetpoints"))
-      return "ka50TGT";
-    else {
-      return TwoOptionsDialog({
-        title: "Enter as?",
-        op1: "Waypoints",
-        op2: "Targetpoints",
-      }).then((option) =>
-        option === "Targetpoints" ? "ka50TGT" : "ka50WPT"
-      );
-    }
+  // Ka-50
+  else if (module === "Ka-50" || module === "Ka-50_3") {
+    if (moduleSpecificPreferences?.includes("Waypoints")) return "ka50WPT";
+    if (moduleSpecificPreferences?.includes("Targetpoints")) return "ka50TGT";
+    const option = await TwoOptionsDialog({
+      title: "Enter as?",
+      op1: "Waypoints",
+      op2: "Targetpoints",
+    });
+    return option === "Targetpoints" ? "ka50TGT" : "ka50WPT";
+  }
 
-
-  } else return module;
+  // default
+  else return module;
 };
 
 export default askUserAboutSeat;
