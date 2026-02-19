0ed793168eacc57ae2f5c1e1aaa7839d505990d9
jonsky752 <jonsky7@ntlworld.com>
2026-02-19 16:05:29 +0000

    2.5.16

--- electron/MainWindow.js
+++ electron/MainWindow.js
@@ -22,6 +22,7 @@ class MainWindow extends BrowserWindow {
     });
 
     this.setMenu(null);
+
     if (process.platform === "win32") {
       const WM_INITMENU = 0x0116;
       this.hookWindowMessage(WM_INITMENU, () => {
@@ -29,27 +30,46 @@ class MainWindow extends BrowserWindow {
         this.setEnabled(true);
       });
     }
+
     this.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+
+    // Keep this initial call (helps in many cases)
     this.setAlwaysOnTop(true, "screen-saver");
+
+    // Re-assert always-on-top AFTER the window is actually visible
+    const reassertOnTop = () => {
+      this.setAlwaysOnTop(true, "screen-saver");
+    };
+
+    this.on("show", reassertOnTop);
+    this.on("restore", () => {
+      this.setFocusable(false);
+      reassertOnTop();
+    });
+    this.on("blur", reassertOnTop); // optional, but helps with fullscreen games
 
     ipcMain.on("minimize", () => {
       this.setFocusable(true);
       this.minimize();
     });
+
     ipcMain.on("close", () => {
       this.close();
     });
+
     ipcMain.on("focus", () => {
       this.setFocusable(true);
+      reassertOnTop();
     });
+
     ipcMain.on("defocus", () => {
       this.setFocusable(false);
+      reassertOnTop();
     });
-    this.on("restore", () => {
-      this.setFocusable(false);
-    });
+
     this.on("ready-to-show", () => {
       this.show();
+      reassertOnTop(); // extra nudge right after showing
     });
   }
 }
