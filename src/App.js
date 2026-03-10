23bd46f98914057f67eaa69a556bc076329efb03
jonsky752 <jonsky7@ntlworld.com>
2026-03-10 22:31:52 +0000

    2.5.19

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import {
-  Box,
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
-} from "@mui/material";
-
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ModalContainer from "react-modal-promise";
@@ -19,13 +13,9 @@ import GetModuleCommands from "./moduleCommands/GetModuleCommands";
 import askUserAboutSeat from "./moduleCommands/askUserAboutSeat";
 import useElectronIpcListeners from "./hooks/useElectronIpcListeners";
 import SettingsDialog from "./components/settings/SettingsDialog";
-import UnitImportDialog from "./components/UnitImportDialog";
 import { uiActions } from "./store/ui";
 
 const { ipcRenderer } = window.require("electron");
-
-const isUnitImportWindow =
-  new URLSearchParams(window.location.search).get("window") === "unitImport";
 
 const theme = createTheme(theWayTheme);
 
@@ -35,7 +25,7 @@ function App() {
   const userPreferences = useSelector((state) => state.ui.userPreferences);
   const [settingsModalOpen, setSettingsModalOpen] = useState(false);
   const [inputMethod, setInputMethod] = useState("F10 Map");
-const [isSelecting, setIsSelecting] = useState(false);
+  const [isSelecting, setIsSelecting] = useState(false);
   const buttonExtraDelay = userPreferences["buttonDelay"] ?? 0;
   const oldCrosshair = userPreferences["oldCrosshair"];
   const dispatch = useDispatch();
@@ -60,58 +50,46 @@ const [isSelecting, setIsSelecting] = useState(false);
     ipcRenderer.send("saveFile", JSON.stringify(dcsWaypoints));
   }, [dcsWaypoints]);
 
- const handleSelectionToggle = useCallback(() => {
-  if (!isSelecting) {
-    if (inputMethod === "F10 Map") {
-      if (oldCrosshair) {
-        ipcRenderer.send("f10Start");
-      } else {
-        ipcRenderer.send("messageToDcs", {
-          type: "crosshair",
-          payload: "true",
-        });
+  const handleSelectionToggle = useCallback(() => {
+    if (!isSelecting) {
+      if (inputMethod === "F10 Map") {
+        if (oldCrosshair) {
+          ipcRenderer.send("f10Start");
+        } else {
+          ipcRenderer.send("messageToDcs", {
+            type: "crosshair",
+            payload: "true",
+          });
+        }
+        dispatch(uiActions.changePendingWaypoint(true));
+        setIsSelecting(true);
+      } else if (inputMethod === "From a file") {
+        ipcRenderer.send("openFile");
       }
-      dispatch(uiActions.changePendingWaypoint(true));
-      setIsSelecting(true);
-    } else if (inputMethod === "From a file") {
-      ipcRenderer.send("openFile");
-    } else if (inputMethod === "Recon Request") {
-      ipcRenderer.send("openUnitImport");
-    }
-  } else {
-    if (inputMethod === "F10 Map") {
-      if (oldCrosshair) {
-        ipcRenderer.send("f10Stop");
-      } else {
-        ipcRenderer.send("messageToDcs", {
-          type: "crosshair",
-          payload: "false",
-        });
+    } else {
+      if (inputMethod === "F10 Map") {
+        if (oldCrosshair) {
+          ipcRenderer.send("f10Stop");
+        } else {
+          ipcRenderer.send("messageToDcs", {
+            type: "crosshair",
+            payload: "false",
+          });
+        }
+        dispatch(uiActions.changePendingWaypoint(false));
+        setIsSelecting(false);
       }
-      dispatch(uiActions.changePendingWaypoint(false));
-      setIsSelecting(false);
     }
-  }
-}, [isSelecting, inputMethod, oldCrosshair]);
-
+  }, [isSelecting, inputMethod, oldCrosshair]);
 
   useEffect(() => {
     ipcRenderer.on("transferWaypoints", handleTransfer);
     ipcRenderer.on("toggleCrosshair", handleSelectionToggle);
-  return () => {
+    return () => {
       ipcRenderer.removeAllListeners("transferWaypoints");
       ipcRenderer.removeAllListeners("toggleCrosshair");
     };
   }, [handleTransfer, handleSelectionToggle]);
-
-  if (isUnitImportWindow) {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <UnitImportDialog open={true} onClose={() => ipcRenderer.send("closeUnitImport")} standalone />
-      </ThemeProvider>
-    );
-  }
 
   return (
     <ThemeProvider theme={theme}>
